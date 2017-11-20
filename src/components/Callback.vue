56ccd495cd071d1f481fc2477eba041381a8adce
ratsjosh <ratsjosh@outlook.com>
2017-11-20 13:29:42 +0800

    Hotfix: Add Config file

--- src/components/Callback.vue
+++ src/components/Callback.vue
@@ -15,17 +15,16 @@
   }
 </script>
 
-<style>
-  .spinner {
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: white;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
+<style <style lang="sass" scoped>
+  .spinner 
+    position: absolute
+    display: flex
+    justify-content: center
+    height: 100vh
+    width: 100vw
+    background-color: white
+    top: 0
+    bottom: 0
+    left: 0
+    right: 0
 </style>
