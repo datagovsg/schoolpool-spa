d23395aa69451569c82736f284582b888cbab2ff
ratsjosh <ratsjosh@outlook.com>
2017-11-22 08:47:21 +0800

    Settings page milestone
    TODO:
    ~ Implement Google maps

---
+++ src/components/Settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <GoogleMaps></GoogleMaps>
+</template>
+
+<script>
+
+import GoogleMaps from './GoogleMaps'
+
+export default {
+  components: {
+    GoogleMaps,
+  },
+}
+</script>
+
