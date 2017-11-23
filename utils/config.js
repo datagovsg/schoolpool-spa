565126901c46159db23d3cfcd13db3696dff4cee
ratsjosh <ratsjosh@outlook.com>
2017-11-23 16:35:47 +0800

    Major changes
    ~ Decoupled dropdown component
    ~ Changed form structure

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,10 @@ const config = {
   googleMaps: {
     key: 'AIzaSyAm8UrU_IlJ1ZIcSUeESFm7HfeYELf6F-w',
   },
+  govData: {
+    serverURL: 'https://data.gov.sg/api/action/datastore_search',
+    schoolEndpoint: 'ede26d32-01af-4228-b1ed-f05c45a1d8ee',
+  },
   auth0Lock: {
     clientID: '9cvkRePcJ9271Uo46NgwJjxJBPotOaHZ',
     domain: 'schoolpool.auth0.com',
