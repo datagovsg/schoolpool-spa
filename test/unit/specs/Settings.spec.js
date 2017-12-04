f7587271cefd453663ed3aff7788978bfb0a8223
ratsjosh <ratsjosh@outlook.com>
2017-12-04 17:04:59 +0800

    Auto stash before merge of "develop" and "origin/develop"
    ~ Moved component wrapper declaration
    ~ Added IDs to Settings template for form validation

--- test/unit/specs/Settings.spec.js
+++ test/unit/specs/Settings.spec.js
@@ -7,14 +7,13 @@ import User from '../../../src/specs/models/User'
 
 describe('Settings.vue', () => {
   const propsData = { profile: new User('Joshua', '1 Normanton Park 118998', 1.03, 101.02, ['139469']) }
+  const wrapper = mount(Settings, { propsData })
 
   it('has a root element with class .columns', () => {
-    const wrapper = mount(Settings, { propsData })
     expect(wrapper.is('.columns')).to.equal(true)
   })
 
   it('should contain 5 form elements', () => {
-    const wrapper = mount(Settings, { propsData })
     const divElements = wrapper.findAll('form div.field')
     expect(divElements.length).to.equal(5)
   })
@@ -22,7 +21,6 @@ describe('Settings.vue', () => {
 
   it('calls updateUserInformation before mount', (done) => {
     const spy = sinon.spy(Settings.methods, 'updateUserInformation')
-    mount(Settings, { propsData })
     Vue.nextTick(() => {
       try {
         done()
