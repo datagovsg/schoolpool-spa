1f78b6e694a3e5463f860b255b902acb7b70ba1b
ratsjosh <ratsjosh@outlook.com>
2017-12-18 16:39:11 +0800

    Hotfix: Change karma.conf file

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
     // 2. add it to the `browsers` array below.
     browsers: [
       'PhantomJS',
-      // 'Chrome',
+      'Chrome',
       // --browsers=Chrome --single-run=false --auto-watch,
     ],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
