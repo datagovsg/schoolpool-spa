5123d2bd361d474ad0af32070dff2e1d6a38fc25
soraino <personalstuff97@gmail.com>
2017-12-20 15:54:45 +0800

    update .travis.yml file

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
     // 2. add it to the `browsers` array below.
     browsers: [
       'PhantomJS',
-      // 'Chrome',
+      'Chrome',
     ],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
